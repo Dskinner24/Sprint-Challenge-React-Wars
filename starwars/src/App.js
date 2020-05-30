1ef576f5138ba405801c520feffe52eb1ecc0083
unknown <dskinner_93@yahoo.com>
2020-05-29 20:14:29 -0700

    Sprint Challenge

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,17 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Character from './components/Character';
 import './App.css';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    axios
+    .get('https://rickandmortyapi.com/api/character')
+    .then(res => {
+      // console.log(res.data.results)
+      setData(res.data.results);
+      
+    })
+  }, [])
+  // console.log(data);
+
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      <Character characterArray={data}/>
     </div>
   );
 }
